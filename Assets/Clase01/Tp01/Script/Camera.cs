d10ccb2bb6d7b69a427862baf0f3db8c485f7225
Cristian <cusumdt@gmail.com>
2019-04-01 09:24:08 -0300

    add project

---
+++ Assets/Clase01/Tp01/Script/Camera.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    bool castSwitch=true;
+    int planet=0;
+    float x=0;
+    float z=0;
+    float y=0;
+   // public int cant;
+    public GameObject[] SolarSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (planet < SolarSystem.Length)
+        {
+            //Preguntar
+             /*   x = transform.position.x + Time.deltaTime*0.3f;
+                if (x>= SolarSystem[planet].transform.position.x-4)
+                {
+                    x = SolarSystem[planet].transform.position.x-4;
+                }
+                transform.position = new Vector3(x, 4.61f, SolarSystem[planet].transform.position.z);
+                */
+            transform.position = new Vector3(SolarSystem[planet].transform.position.x - 4, 4.61f, SolarSystem[planet].transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(-28.6f, 18.14f, -11.67f);
+        }
+        StartCoroutine("switchCamera");
+    }
+
+    private IEnumerator switchCamera()
+    {
+        if (castSwitch)
+        {
+            castSwitch = false;
+            yield return new WaitForSeconds(5.0f);
+            planet++;
+            if (planet==SolarSystem.Length+1) {
+                planet = 0;
+            }
+            castSwitch = true;
+        }
+    }
+}
---
+++ Assets/Clase01/Tp01/Script/Planeta.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planeta : MonoBehaviour
+{
+    public GameObject sun;
+    public float distance;
+    public float velocity;
+    float x;
+    float z;
+    float alpha;
+    float smooth = 5.0f;
+    float tiltAngle = 60.0f;
+    public float velocityRotation;
+    float alphaRotation;
+
+    void Start()
+    {
+        transform.position = new Vector3(distance, transform.position.y, transform.position.z);
+    }
+
+    void Update()
+    {
+
+        //Rotation system 31/03/2019 17:00hs
+        alphaRotation = alphaRotation + Time.deltaTime * velocityRotation;
+        float tiltAroundZ = alphaRotation * tiltAngle;
+        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+        Quaternion target = Quaternion.Euler(tiltAroundX, tiltAroundZ, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+        alpha = alpha + Time.deltaTime * velocity;
+        //Transform Position system 25/03/2019 12:30hs
+        x = sun.transform.position.x + distance * Mathf.Cos(alpha);
+        z = sun.transform.position.z + distance * Mathf.Sin(alpha);
+        transform.position = new Vector3(x, transform.position.y, z);
+    }
+}
